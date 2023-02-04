a25ce14c58d88523097a970c87355ad5f6f3d67d
Noam Zadok <noam.z1@cs.huji.ac.il>
2023-02-04 18:29:38 +0200

    Follow react conventions

--- src/components/ExpenseAddFrom.tsx
+++ src/components/ExpenseAddFrom.tsx
@@ -8,15 +8,15 @@ export default function ExpenseAddFrom() {
     return (
         <>
             <h1> My Form</h1>
-            <div class="expense-input" id="expense-name">
+            <div className="expense-input" id="expense-name">
                 <label>Name</label>
                 <input ref={expenseName} type="text" />
             </div>
-            <div class="expense-input" id="expense-value">
+            <div className="expense-input" id="expense-value">
                 <label>Sum</label>
                 <input ref={expenseValue} type="text" />
             </div>
-            <div class="expense-input" id="expense-value">
+            <div className="expense-input" id="expense-value">
                 <label>Date</label>
                 <input ref={expenseDate} type="text" />
             </div>
