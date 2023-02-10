cfaa17408fbb06f15c88a83cdfd001f7d0a73a8c
Noam Zadok <noam.z1@cs.huji.ac.il>
2023-02-10 17:32:30 +0200

    Use component as a function and not a class

--- src/components/ExpenseAddFrom.tsx
+++ src/components/ExpenseAddFrom.tsx
@@ -1,31 +1,28 @@
-import React, { Component, useRef } from 'react';
-import useConfig from "./useConfig";
+import React, { useRef } from 'react';
 
-export class ExpenseAddFrom extends Component {
-    
-    render() {
-        const expenseName = useRef();
-        const expenseValue = useRef();
-        const expenseDate = useRef();
-        return (
-            <>
-            <h1> My Form</h1>
-            <div className="expense-input" id="expense-name">
-                <label>Name</label>
-                <input ref={expenseName} type="text" />
-            </div>
-            <div className="expense-input" id="expense-value">
-                <label>Sum</label>
-                <input ref={expenseValue} type="text" />
-            </div>
-            <div className="expense-input" id="expense-value">
-                <label>Date</label>
-                <input ref={expenseDate} type="text" />
-            </div>
-            <button>Add expense</button>
-            <div>
-            </div>
-        </>
-        )
-    }
+export default function ExpenseAddFrom() {
+    const expenseName = useRef();
+    const expenseValue = useRef();
+    const expenseDate = useRef();
+    return (
+        <>
+        <h1> My Form</h1>
+        <div className="expense-input" id="expense-name">
+            <label>Name</label>
+            <input ref={expenseName} type="text" />
+        </div>
+        <div className="expense-input" id="expense-value">
+            <label>Sum</label>
+            <input ref={expenseValue} type="text" />
+        </div>
+        <div className="expense-input" id="expense-value">
+            <label>Date</label>
+            <input ref={expenseDate} type="text" />
+        </div>
+        <button>Add expense</button>
+        <div>
+            {/* <h1>{JSON.stringify(this.state)}</h1> */}
+        </div>
+    </>
+    )
 }
