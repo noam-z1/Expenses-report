77594e9af4fa84c6ec0e4acf499a14db1f1be0b1
Noam Zadok <noam.z1@cs.huji.ac.il>
2023-02-19 19:47:28 +0200

    useState for category

--- src/components/ExpensesCategories.tsx
+++ src/components/ExpensesCategories.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function ExpensesCategories({ expenseCategories }: { expenseCategories: string[] }) {
+
+    const [expensesCategoriesValue, setExpenseCategoryChoice] = useState('');
+
+    const HandleChange = (e: any) => {
+
+        setExpenseCategoryChoice(e.target.value);
+    }
+
     if (expenseCategories.length == 0) {
         return (
             <>
             <label>Category</label>
-            <select id="category">
-                <option>Loading...</option>
+            <select id="category"  value={expensesCategoriesValue} onChange={HandleChange}>
+                <option value="">Loading...</option>
             </select>
             </>
         )
@@ -15,7 +23,8 @@ export default function ExpensesCategories({ expenseCategories }: { expenseCateg
     return (
         <>
             <label>Category</label>
-            <select id="category">
+            <select id="category" value={expensesCategoriesValue}  onChange={HandleChange}>
+                <option value="">Please choose a category</option>
                 {expenseCategories.map(category => {
                     return <option key={category} value={category}>{category}</option>
                 })}
