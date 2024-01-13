d3048c78472d368ed55ba443763761a98da6dbb1
Noam <noam.z1@gmail.com>
2024-01-13 12:12:07 +0200

    Clear category

--- src/components/ExpenseAddFrom.tsx
+++ src/components/ExpenseAddFrom.tsx
@@ -77,6 +77,7 @@ export default function ExpenseAddFrom(
         setExpenseCategoryChoice('');
         setSecondExpenseCategoryChoice('');
         setCheckboxChecked(false);
+        setShowAddCategory(false);
     }
 
     function handleCheckboxChange() {
