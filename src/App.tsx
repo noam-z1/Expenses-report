65ecb1f376b05edceb6f66c3ded2e00b9560f5f9
Noam <noam.z1@gmail.com>
2024-02-03 12:03:12 +0200

    log

--- src/App.tsx
+++ src/App.tsx
@@ -32,6 +32,7 @@ export default function App() {
               }
             );
             const { oldValue, newValue } = response.data;
+            console.log(`response - ${JSON.stringify(response)}`)
             const expenseData: ExpenseData = {
               category: expense.category,
               date: expense.date,
